29cb825af49006cde6de5e4d6036aedf8ec4680d
Luis Pérez <lperez.1092gmail.com>
2017-05-08 05:18:01 -0400

    Agrega ImageView a sección "About"

--- app/src/main/java/com/example/android/clanmanager/AboutActivity.java
+++ app/src/main/java/com/example/android/clanmanager/AboutActivity.java
@@ -1,7 +1,7 @@
 package com.example.android.clanmanager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
 
