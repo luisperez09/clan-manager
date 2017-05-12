92fbde369b209b787acc58cd8b863ac99ebedae2
Luis Pérez <lperez.1092gmail.com>
2017-05-11 23:37:44 -0400

    Agrega BannedActivity. Agrega Strings y estilos relacionados

---
+++ app/src/main/java/com/example/android/clanmanager/BannedActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.clanmanager;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BannedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_banned);
+    }
+}
--- app/src/main/java/com/example/android/clanmanager/BlacklistActivity.java
+++ app/src/main/java/com/example/android/clanmanager/BlacklistActivity.java
@@ -1,13 +1,26 @@
 package com.example.android.clanmanager;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class BlacklistActivity extends AppCompatActivity {
+
+    private FloatingActionButton mAddBannedFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blacklist);
+
+        mAddBannedFab = (FloatingActionButton) findViewById(R.id.fab);
+        mAddBannedFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(BlacklistActivity.this, BannedActivity.class));
+            }
+        });
     }
 }
