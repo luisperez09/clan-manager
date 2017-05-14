0598106e6e002d5a6cc9b4f52f3746d2703fcb18
Luis Pérez <lperez.1092gmail.com>
2017-05-13 20:49:54 -0400

    Agrega StrikeAdapter y muestra Listview con datos placeholder. Agrega parent al Editor.

--- app/src/main/java/com/example/android/clanmanager/SancionesEditorActivity.java
+++ app/src/main/java/com/example/android/clanmanager/SancionesEditorActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,11 +12,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class SancionesEditorActivity extends AppCompatActivity {
 
     private TextView mSancionadoTextView;
+    private StrikeAdapter mStrikeAdapter;
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mUserStrikesReference;
 
@@ -26,21 +29,27 @@ public class SancionesEditorActivity extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         final String key = extras.getString("key");
-        setTitle(extras.getString("name"));
+        String username = extras.getString("name");
+        setTitle(username);
 
 
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mUserStrikesReference = mFirebaseDatabase.getReference().child("sancionados").child(key).child("strikes");
 
+        ArrayList<Strike> strikes = new ArrayList<>();
+        strikes.add(new Strike("23/323/424", "Se salto una reserva"));
+        strikes.add(new Strike("23/323/523", "Se salto otra reserva"));
+        mStrikeAdapter = new StrikeAdapter(this, strikes);
+        ListView listView = (ListView) findViewById(R.id.strikes_list);
+        listView.setAdapter(mStrikeAdapter);
 
         mSancionadoTextView = (TextView) findViewById(R.id.sancionado_text_view);
+        mSancionadoTextView.setText(username);
         FloatingActionButton button = (FloatingActionButton) findViewById(R.id.agregar_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(SancionesEditorActivity.this, key, Toast.LENGTH_SHORT).show();
                 pushNewStrike();
-
             }
         });
     }
@@ -51,6 +60,4 @@ public class SancionesEditorActivity extends AppCompatActivity {
         mUserStrikesReference.push().setValue(strike);
         Toast.makeText(this, "Se agregó el strike", Toast.LENGTH_SHORT).show();
     }
-
-
 }
---
+++ app/src/main/java/com/example/android/clanmanager/StrikeAdapter.java
@@ -0,0 +1,40 @@
+package com.example.android.clanmanager;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class StrikeAdapter extends ArrayAdapter<Strike> {
+
+    public StrikeAdapter(Context context, ArrayList<Strike> data) {
+        super(context, 0, data);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item,
+                    parent, false);
+        }
+
+        Strike currentStrike = getItem(position);
+
+        String date = currentStrike.getDate();
+        TextView dateTextView = (TextView) listItemView.findViewById(R.id.option_text_view);
+        dateTextView.setText(date);
+
+        String reason = currentStrike.getReason();
+        TextView reasonTextView = (TextView) listItemView.findViewById(R.id.summary_text_view);
+        reasonTextView.setText(reason);
+
+        return listItemView;
+    }
+}
